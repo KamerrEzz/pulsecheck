394ffcf0c5234a683276d7894fb16ce8fb1dc6c2
Kamerr Ezz <kamerr_oficial@hotmail.com>
2025-12-28 22:30:14 -0500

    feat(service.controller): add history retrieval to service enrichment
    Fetch service history from Redis and include it in the enriched service data returned to the client

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -52,7 +52,8 @@ serviceController.listPartial = async (req, res) => {
         const enrichedServices = await Promise.all(services.map(async (service) => {
             const status = await redisClient.get(`service:${service.id}:status`) || service.status;
             const responseTime = await redisClient.get(`service:${service.id}:responseTime`);
-            return { ...service, status, responseTime };
+            const history = await redisClient.lRange(`service:${service.id}:history`, 0, -1) || [];
+            return { ...service, status, responseTime, history };
         }));
 
         res.render('partials/service-list', { layout: false, services: enrichedServices });
